fe2ef485d20fed01a4ba2633dcb30eadc792ddca
sidd2395 <sidd2395@gmail.com>
2021-03-05 14:54:49 +0800

    Fixed some conflicts on app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import Balance from './components/balance/balance';
-
+import Nav from './components/nav/nav';
 import Login from './components/login/login';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        
+        <Nav></Nav>
         <Route path="/balance" component={Balance}></Route>
         <Route exact={true} path='/'>
-            <Login />
-          </Route>
+          <Login />
+        </Route>
       </Router>
     </div>
   );
