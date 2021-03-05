0d00c637cad294da66804c4c902f7745baa68d40
DESKTOP-7VDSOD8\chloehojialin <katie-1511@hotmail.com>
2021-03-05 16:06:39 +0800

    Added getbalance page

--- src/components/balance/balance.js
+++ src/components/balance/balance.js
@@ -1,52 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 const Balance = () => {
   const [assetBalance, setAssetBalance] = useState("");
   const [cashBalance, setCashBalance] = useState("");
 
-  const submit = async (e) => {
+  const load = async (e) => {
     e.preventDefault();
 
-    //sent by teammate
-    const payload = {
-      accountKey: "ca9b610a-3752-435b-be40-3311b200fab1",
-    };
-    let config = {
-      headers: {
-        "x-api-key": "2kQs4oNvqe91cr9AdfHis28uFrUeobspaUtLslE0",
-      },
-    };
-    await axios
-      .post(
-        "https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance",
-        payload,
-        config
-      )
-      .then((response) => {
-        console.log(response.data);
-      })
-
-      // await fetch('https://849rs099m3.excute-api.ap-southeast-1.amazonaws.com/techtrek/balance', {
-      //     method: 'POST',
-      //     headers: {
-      //         'x-api-key': '2kQs4oNvqe91cr9AdfHis28uFrUeobspaUtLslE0'
-      //     },
-      //     body: JSON.stringify({
-      //         'accountKey' : 'ca9b610a-3752-435b-be40-3311b200fab1'
-      //         })
-
+    await fetch(
+      "https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance",
+      {
+        method: "POST",
+        headers: {
+          "x-api-key": "2kQs4oNvqe91cr9AdfHis28uFrUeobspaUtLslE0",
+        },
+        body: JSON.stringify({
+          accountKey: "ca9b610a-3752-435b-be40-3311b200fab1",
+        }),
+      }
+    )
       .then((response) => response.json())
       .then((response) => {
-        setId(response.data.id);
-        setId(response.data.id);
+        // do something with the data
+        console.log(response);
+        setAssetBalance(response.assetBalance);
+        setCashBalance(response.cashBalance);
       });
   };
 
   return (
-    <body onload={submit}>
-      <p className="cash">Cash balance:{}</p>
-      <p className="asset">Asset balance:{}</p>
-    </body>
+    <div style={{ marginTop: "10%" }}>
+      <button onClick={load}>test</button>
+      <p className="cash">Cash balance:{assetBalance}</p>
+      <p className="asset">Asset balance:{cashBalance}</p>
+    </div>
   );
 };
 
