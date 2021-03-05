451b60697623158dd33da7a3943f4467ef3e3546
Chris <chris888222@gmail.com>
2021-03-05 13:51:45 +0800

    minor changes to trans

--- src/components/transaction/transaction.js
+++ src/components/transaction/transaction.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
+const payload = { accountKey: "ca9b610a-3752-435b-be40-3311b200fab1" };
 let config = {
   headers: {
     "x-api-key": "2kQs4oNvqe91cr9AdfHis28uFrUeobspaUtLslE0",
@@ -10,7 +11,7 @@ let config = {
 };
 
 function Transaction() {
-  const [isLoading, setisLoading] = useState(true);
+  //const [isLoading, setisLoading] = useState(true);
   const [update, setUpdate] = useState([]);
 
   /*const data = {
@@ -65,7 +66,7 @@ function Transaction() {
     axios
       .post(
         "https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view",
-        //payload,
+        payload,
         config
       )
       .then((data) => {
@@ -77,12 +78,14 @@ function Transaction() {
     viewPast();
   }, []);
 
-  if (isLoading) {
+  //isLoading is always active.. removed for now
+  /*if (isLoading) {
     return <div>Loading...</div>;
-  }
+  }*/
 
   return (
     <div className="history">
+      <br /> <br /> <br /> <br /> <br />
       <h2>Transaction History</h2>
       <table striped bordered hover>
         <thead>
