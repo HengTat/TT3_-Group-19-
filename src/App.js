fa02b8f26d641e951247f88ef6ae585b1d4cca70
Chonght <60685286+HengTat@users.noreply.github.com>
2021-03-05 13:50:44 +0800

    first commit for branch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import Balance from './components/balance/balance';
 import Nav from './components/nav/nav';
-<<<<<<< HEAD
 import Login from './components/login/login';
-=======
 import BuyorSell from './components/buyorsell/buyorsell';
->>>>>>> buyorsell
 
 function App() {
   return (
@@ -15,13 +12,10 @@ function App() {
       <Router>
         <Nav></Nav>
         <Route path="/balance" component={Balance}></Route>
-<<<<<<< HEAD
         <Route exact={true} path='/'>
             <Login />
           </Route>
-=======
         <Route path="/buyorsell" component={BuyorSell}></Route>
->>>>>>> buyorsell
       </Router>
     </div>
   );
