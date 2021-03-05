d080d319dc790ed57a1c67e036070c248f16c695
Chris <chris888222@gmail.com>
2021-03-05 12:01:49 +0800

    Test transaction page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
-import Balance from './components/balance/balance';
-import Nav from './components/nav/nav';
+import Balance from "./components/balance/balance";
+import Nav from "./components/nav/nav";
+import Transaction from "./components/transaction/transcation";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Nav></Nav>
         <Route path="/balance" component={Balance}></Route>
+        <Route path="/transaction" component={Transaction}></Route>
       </Router>
     </div>
   );
